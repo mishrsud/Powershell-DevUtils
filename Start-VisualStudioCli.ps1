04fe3763a945e35f185653e35ee3cbb3363f51dc
Sudhanshu Mishra <mishrsud@users.noreply.github.com>
2017-07-10 16:01:14 +1000

    Updated with example

--- Start-VisualStudioCli.ps1
+++ Start-VisualStudioCli.ps1
@@ -1,6 +1,7 @@
 # to get sudo: choco install sudo
 # Import this script in your starup as detailed here: https://social.technet.microsoft.com/Forums/windowsserver/en-US/7901f9bd-674c-4298-93ed-f997c46541f4/start-powershell-with-modules-loaded?forum=winserverpowershell
 # powershell profile is in $Home\Documents\WindowsPowerShell\ 
+# E.g.: Import-Module C:\Users\<username>\Documents\WindowsPowerShell\Modules\mine\Start-VisualStudioCli.ps1
 # once done, you can simply do vsd . and visual studio will open the solution in current directory (as admin)
 function vsd { 
     $vs = "c:\program files (x86)\microsoft visual studio\2017\professional\common7\ide\devenv.exe"
